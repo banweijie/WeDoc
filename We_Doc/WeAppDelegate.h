f8c829858ded3767a76686efb63e36e81b8b92dc
ejren <baifenghua@baifenghuamatoMacBook-Air.local>
2014-10-24 12:11:16 +0800

    修改了分享链接

--- We_Doc/WeAppDelegate.h
+++ We_Doc/WeAppDelegate.h
@@ -206,7 +206,7 @@ weTargetView we_targetView;
 #define yijiarenImageThumbUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"imageThumbPath"], fileName]
 
 #define yijiarenAudioUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"audioPath"], fileName]
-#define yijiarenShareURL(field) [NSString stringWithFormat:@"%@/crowdfunding.jsp?fundingId=%@", yijiarenServer, field]
+#define yijiarenShareURL(field) [NSString stringWithFormat:@"%@/web/viewFunding?fundingId=%@", yijiarenServer, field]
 #define yijiarenVideoUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"videoPath"], fileName]
 
 #define UIColorFromRGB0x(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
